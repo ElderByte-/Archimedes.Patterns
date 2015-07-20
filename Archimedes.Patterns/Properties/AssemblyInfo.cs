850890bc6fd573e4823e138098abde3771315c3e
IsNull <pascalbuettiker@bluewin.ch>
2015-07-20 11:44:18 +0200

    extracted archimedes pattern code now compiling

--- Archimedes.Patterns/Properties/AssemblyInfo.cs
+++ Archimedes.Patterns/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Archimedes.Patterns")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("37330a91-37a8-4737-a5be-fa49b1fb7928")]
+[assembly: Guid("dd21c043-614a-4532-8837-9b83a5e3c78e")]
 
 // Version information for an assembly consists of the following four values:
 //
