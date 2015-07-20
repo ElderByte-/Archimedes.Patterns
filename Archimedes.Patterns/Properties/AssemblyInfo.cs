4b16d21efa0bead27a12afb77c676ac30ff44961
IsNull <pascalbuettiker@bluewin.ch>
2015-07-20 12:03:15 +0200

    sign the assembly for strong name

--- Archimedes.Patterns/Properties/AssemblyInfo.cs
+++ Archimedes.Patterns/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Archimedes.Patterns")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ElderByte")]
 [assembly: AssemblyProduct("Archimedes.Patterns")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Archimedes")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
